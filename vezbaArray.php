4e6f7956681ddf8ac634c459522f992bfe55dc99
Aleksandar Radovanović <radovanovic.sale1996@gmail.com>
2026-09-02 23:22:04 +0200

    Added vezba

---
+++ vezbaArray.php
@@ -0,0 +1,9 @@
+<?php
+
+    $products = ["pc ", "monitor ", "keyboard "];
+
+    echo $products[0];
+    echo $products[1];
+    echo $products[2];
+
+?>
---
+++ vezbaArray1.php
@@ -0,0 +1,10 @@
+<?php
+
+    $mobile = ["samsung ", "nokia ", " alcatel"];
+    $mobile[1] = "sony";
+
+    echo $mobile[0];
+    echo $mobile[1];
+    echo $mobile[2];
+
+?>
