4d13549d70da7869dee2ebb803aaa385f21ce8e5
Aleksandar Radovanović <radovanovic.sale1996@gmail.com>
2026-09-02 23:31:54 +0200

    Added vezbaArray2

---
+++ vezbaArray2.php
@@ -0,0 +1,15 @@
+<?php
+
+$voce = ["jabuka ", "kruska ", " mandarina "];
+$povrce = [" krompir ", " luk", " sargarepa"];
+
+$hrana = array_merge($voce, $povrce);
+
+echo $hrana [0];
+echo $hrana [1];
+echo $hrana [2];
+echo $hrana [3];
+echo $hrana [4];
+echo $hrana [5];
+
+?>
